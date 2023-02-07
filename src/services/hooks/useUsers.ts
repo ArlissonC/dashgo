850e6ee0fd6b59bf977c0cf4fccc9dfa8fbfa92a
Arlisson Costa <arlisson.fs13@gmail.com>
2023-02-07 00:09:21 -0300

    separating function getUsers

--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -1,23 +1,30 @@
 import { useQuery } from "react-query";
 import { api } from "../api";
 
-export const useUsers = () => {
-  return useQuery("users", async () => {
-    const { data } = await api.get("users");
-
-    const users = data.users.map((user: any) => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
-          day: "2-digit",
-          month: "long",
-          year: "numeric",
-        }),
-      };
-    });
-
-    return users;
-  });
+type User = {
+  id: string;
+  name: string;
+  email: string;
+  createdAt: string;
 };
+
+export const getUsers: () => Promise<User[]> = async () => {
+  const { data } = await api.get("users");
+
+  const users = data.users.map((user: any) => {
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }),
+    };
+  });
+
+  return users;
+};
+
+export const useUsers = () => useQuery("users", getUsers);
