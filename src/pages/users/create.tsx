30b897c76a16bb1af7b2de580ea20dde44d12134
Arlisson Costa <arlisson.fs13@gmail.com>
2023-02-11 03:02:29 -0300

    useMutation

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -16,6 +16,10 @@ import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { Input } from "@/components/Form/Input";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { useMutation } from "react-query";
+import { api } from "@/services/api";
+import { queryClient } from "@/services/queryClient";
+import { useRouter } from "next/router";
 
 const createUserFormSchema = yup.object().shape({
   name: yup.string().required("Nome obrigatório"),
@@ -29,14 +33,44 @@ const createUserFormSchema = yup.object().shape({
     .oneOf([null, yup.ref("password")], "As senhas precisam ser iguais"),
 });
 
+type CreateUserFormData = {
+  name: string;
+  email: string;
+  password: string;
+  password_confirmation: string;
+};
+
 const CreateUser = () => {
+  const router = useRouter();
+  const createUser = useMutation(
+    async (user: CreateUserFormData) => {
+      const response = await api.post("users", {
+        user: {
+          ...user,
+          created_at: new Date(),
+        },
+      });
+
+      return response.data.user;
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries("users");
+      },
+    },
+  );
+
   const { register, handleSubmit, formState } = useForm({
     resolver: yupResolver(createUserFormSchema),
   });
 
   const { errors } = formState;
 
-  const handleCreateUser: SubmitHandler<FieldValues> = (values) => {};
+  const handleCreateUser: SubmitHandler<FieldValues> = async (values) => {
+    await createUser.mutateAsync(values as CreateUserFormData);
+
+    router.push("/users");
+  };
 
   return (
     <Box>
